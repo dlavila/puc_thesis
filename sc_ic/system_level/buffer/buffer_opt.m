81ebc1569d65f571ed645a6561e7292f9dc1d4e3
Diego Avila <diegoavilagarate@gmail.com>
2013-09-12 18:36:41 -0300

    minor changes

--- sc_ic/system_level/buffer/buffer_opt.m
+++ sc_ic/system_level/buffer/buffer_opt.m
@@ -2,21 +2,25 @@ clc
 clear
 
 current_dir         = pwd;
-pwd_levels          = strfind(pwd,'/');
+if isunix
+    sl = '/';
+else
+    sl = '\';
+end
+pwd_levels          = strfind(pwd,sl);
 project_folder      = current_dir(1:(pwd_levels(end-2)-1));
-path_gmId_curves    = [project_folder '/umc180_gmId_curves'];
-path_switch_curves  = [project_folder '/umc180_switch_curves'];
+path_gmId_curves    = [project_folder sl 'umc180_gmId_curves'];
+path_switch_curves  = [project_folder sl 'umc180_switch_curves'];
 
 addpath(path_gmId_curves);
 addpath(path_switch_curves);
 
 load_data_gmId;
-load_data_switch
 
 
 Vdd     = 1.8;
-
-Vocm = Vdd/2;
+Vds     = Vdd/2;
+Vocm    = Vdd/2;
 
 
 %   Min_n
@@ -29,28 +33,58 @@ Vocm = Vdd/2;
 %   Mout_p
 
 CL      = 8e-12;
+L = 0.45;
 
-It = 
-Ic = 
-Il =
+It = 60e-6;
+Ic = 60e-6;
+Il = 90e-6;
+Iab = 240e-6;
+
+gmId_in = 18;
+gmId_ip = 17;
+gmId_lp = 12.2;
+gmId_ln = 12.3;
+gmId_cp = 14;
+gmId_cn = 14;
+gmId_outn = 9.6;
+gmId_outp = 9.7;
 
 
+gm_in = gmId_in*It/4; 
+gm_ip = gmId_ip*It/4;
 
-%%
+gm_cp = gmId_cp*Ic/2;
+gm_cn = gmId_cn*Ic/2;
 
-gm_in =  (gm_in_n + gm_in_p)/2;
+gm_outn = gmId_outn*Iab;
+gm_outp = gmId_outp*Iab;
+
+ro_in = ro(gmId_in, 0, Vdd/2, L, 'n');
+ro_ip = ro(gmId_ip, 0, Vdd/2, L, 'p');
+ro_ln = ro(gmId_ln, 0, Vds, L, 'n');
+ro_lp = ro(gmId_lp, 0, Vds, L, 'p');
+ro_cn = ro(gmId_cn, 0, Vds, L, 'n');
+ro_cp = ro(gmId_cp, 0, Vds, L, 'p');
+ro_outn = ro(gmId_outn, 0, Vocm, L, 'n');
+ro_outp = ro(gmId_outp, 0, Vocm, L, 'p');
 
 
-Ra = gm_c_p*r0_c_p*par(r0_f_p, r0_in_n);
-Rb = gm_c_n*r0_c_n*par(r0_f_n, r0_in_p);
+gm_in =  gm_in + gm_ip;
+
+
+Ra = gm_cp*ro_cp*par(ro_lp, ro_in);
+Rb = gm_cn*ro_cn*par(ro_ln, ro_ip);
 
 Rout1 = par(Ra,Rb);
 
-gm_out = gm_out_p + gm_out_n;
+gm_out = gm_outp + gm_outn;
 
-Rout2 = par(r0_out_n, r0_out_p);
+Rout2 = par(ro_outn, ro_outp);
 
 Av1 = gm_in*Rout1;
 Av2 = gm_out*Rout2;
 
 A = Av1*Av2;
+
+
+It/4/Id_W(gmId_in,0,Vdd/2,L,'n')
